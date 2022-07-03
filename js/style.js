fb8396c4e1a5da9cb89822256a9ae604ea181d6e
Vladislav Shyrokiy <shyrokij009@gmail.com>
2022-07-03 12:40:52 +0300

    lessons-1

--- js/style.js
+++ js/style.js
@@ -1,1 +1,23 @@
-alert('Hello World!!!');
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+    const a = prompt('Один из последних просмотреных фильмов?', ''),
+          b = prompt('На сколько оцените его?', ''),
+          c = prompt('Один из последних просмотреных фильмов?', ''),
+          d = prompt('На сколько оцените его?', '');
+
+
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
