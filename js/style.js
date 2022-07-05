64619b6503de4dd211eab5aa059b81511ae5e653
Vladislav Shyrokiy <shyrokij009@gmail.com>
2022-07-05 19:56:47 +0300

    lessons-2

--- js/style.js
+++ js/style.js
@@ -10,14 +10,27 @@ const numberOfFilms = +prompt('Сколько фильмов вы уже про�
         privat: false
     };
 
+for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотреных фильмов?', ''),
-          b = prompt('На сколько оцените его?', ''),
-          c = prompt('Один из последних просмотреных фильмов?', ''),
-          d = prompt('На сколько оцените его?', '');
+          b = prompt('На сколько оцените его?', '');
 
-
-
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;
+    }
+          
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
 
     console.log(personalMovieDB);
+ 
